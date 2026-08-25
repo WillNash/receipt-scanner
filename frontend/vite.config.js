981e349aa3cd0ba5b5b0b2da31b0d54d60ffd631
WillNash <willnash@users.noreply.github.com>
2026-08-25 04:14:51 +0000

    fix wasm import 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
-  plugins: [vue(), wasm()],
+  plugins: [vue()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
